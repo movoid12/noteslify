123cd79ceeb3a9c7692269416421d5a304310ee6
Movoid <87415713+movoid12@users.noreply.github.com>
2023-05-12 11:23:59 +0000

    add markdown note editor with save button

---
+++ src/components/noteEditor/NoteEditor.tsx
@@ -0,0 +1,52 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { useState } from "react";
+import { Button, Input, Stack } from "@mantine/core";
+
+import CodeMirror from "@uiw/react-codemirror";
+import { markdown, markdownLanguage } from "@codemirror/lang-markdown";
+import { languages } from "@codemirror/language-data";
+
+const NoteEditor = ({
+  onSave,
+}: {
+  onSave: (note: { title: string; content: string }) => void;
+}) => {
+  const [code, setCode] = useState<string>("");
+  const [title, setTitle] = useState<string>("");
+
+  return (
+    <Stack spacing="md">
+      <Input
+        placeholder="Insert a title for your note"
+        value={title}
+        onChange={(e) => setTitle(e.currentTarget.value)}
+      />
+      <CodeMirror
+        value={code}
+        width="500px"
+        height="30vh"
+        minWidth="100%"
+        minHeight="30vh"
+        extensions={[
+          markdown({ base: markdownLanguage, codeLanguages: languages }),
+        ]}
+        onChange={(value) => setCode(value)}
+        className="border border-gray-300"
+      />
+
+      <Button
+        color="lime"
+        radius="lg"
+        uppercase
+        variant="outline"
+        onClick={() => onSave({ title, content: code })}
+      >
+        Save
+      </Button>
+    </Stack>
+  );
+};
+
+export default NoteEditor;
