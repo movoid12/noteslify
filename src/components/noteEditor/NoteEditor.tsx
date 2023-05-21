1b9f6d01591ed2fda9982d6324ff390fefd4c4bd
Movoid <apothekendatabase@gmail.com>
2023-05-21 23:46:38 +0200

    fix tmp clear content

--- src/components/noteEditor/NoteEditor.tsx
+++ src/components/noteEditor/NoteEditor.tsx
@@ -13,8 +13,8 @@ import Highlight from "@tiptap/extension-highlight";
 import StarterKit from "@tiptap/starter-kit";
 import Underline from "@tiptap/extension-underline";
 import TextAlign from "@tiptap/extension-text-align";
-import { Color } from '@tiptap/extension-color';
-import TextStyle from '@tiptap/extension-text-style';
+import { Color } from "@tiptap/extension-color";
+import TextStyle from "@tiptap/extension-text-style";
 
 const NoteEditor = ({
   onSave,
@@ -48,7 +48,7 @@ const NoteEditor = ({
       Highlight,
       TextAlign.configure({ types: ["heading", "paragraph"] }),
       TextStyle,
-       Color
+      Color,
     ],
     content: code,
     onUpdate: ({ editor }) => {
@@ -84,24 +84,24 @@ const NoteEditor = ({
       <RichTextEditor editor={editor} h={"35vh"} style={{ overflowY: "scroll" }}>
         <RichTextEditor.Toolbar sticky>
           <RichTextEditor.ControlsGroup>
-          <RichTextEditor.ColorPicker
-          colors={[
-            '#25262b',
-            '#868e96',
-            '#fa5252',
-            '#e64980',
-            '#be4bdb',
-            '#7950f2',
-            '#4c6ef5',
-            '#228be6',
-            '#15aabf',
-            '#12b886',
-            '#40c057',
-            '#82c91e',
-            '#fab005',
-            '#fd7e14',
-          ]}
-        />
+            <RichTextEditor.ColorPicker
+              colors={[
+                "#25262b",
+                "#868e96",
+                "#fa5252",
+                "#e64980",
+                "#be4bdb",
+                "#7950f2",
+                "#4c6ef5",
+                "#228be6",
+                "#15aabf",
+                "#12b886",
+                "#40c057",
+                "#82c91e",
+                "#fab005",
+                "#fd7e14",
+              ]}
+            />
             <RichTextEditor.Bold />
             <RichTextEditor.Italic />
             <RichTextEditor.Underline />
@@ -137,7 +137,7 @@ const NoteEditor = ({
           </RichTextEditor.ControlsGroup>
         </RichTextEditor.Toolbar>
 
-        <RichTextEditor.Content mih={"30vh"}/>
+        <RichTextEditor.Content mih={"30vh"} />
       </RichTextEditor>
       <Space />
       <Button
@@ -154,7 +154,7 @@ const NoteEditor = ({
           onSave({ title, content: code });
           setTitle("");
           setCode("");
-          editor.commands.clearContent();
+          // editor.commands.clearContent();
         }}
         disabled={
           title.trim().length === 0 ||
