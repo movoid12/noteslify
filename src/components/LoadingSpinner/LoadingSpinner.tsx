5aabca830c3daacc747780c9a98bb5ca1189078d
Movoid <apothekendatabase@gmail.com>
2023-05-29 21:25:53 +0200

    fix loading state

--- src/components/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -2,15 +2,15 @@ import { Loader } from '@mantine/core';
 import { useHelpers } from '~/helpers/useHelpers';
 
 const LoadingSpinnerNotes = () => {
-  const { noteIsLoading, sessionData } = useHelpers();
+  const { noteIsLoading } = useHelpers();
 
-  return <div>{noteIsLoading && sessionData && <Loader />}</div>;
+  return <div>{noteIsLoading && <Loader />}</div>;
 };
 
 const LoadingSpinnerTopics = () => {
-  const { topicIsLoading, sessionData } = useHelpers();
+  const { topicIsLoading } = useHelpers();
 
-  return <div>{topicIsLoading && sessionData && <Loader />}</div>;
+  return <div>{topicIsLoading && <Loader />}</div>;
 };
 
 export { LoadingSpinnerNotes, LoadingSpinnerTopics };
