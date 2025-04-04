1dc83556e06e53f7e2659a4bb051fc0dfa73a8b9
Mouaz Aldakkak <87415713+movoid12@users.noreply.github.com>
2025-04-04 11:36:41 +0000

    refactor: remove unused example router and clean up imports

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,5 @@
-import { createCallerFactory, createTRPCRouter } from '~/server/api/trpc';
-import { exampleRouter } from '~/server/api/routers/example';
 import { topicRouter } from '~/server/api/routers/topic';
+import { createCallerFactory, createTRPCRouter } from '~/server/api/trpc';
 import { noteRouter } from './routers/note';
 
 /**
@@ -9,7 +8,6 @@ import { noteRouter } from './routers/note';
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   topic: topicRouter,
   note: noteRouter,
 });
