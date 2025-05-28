9aa52b3821ebea070ad997a402cf17d7fa849fff
Mouaz Aldakkak <87415713+movoid12@users.noreply.github.com>
2025-05-28 19:27:46 +0200

    refactor: add user color scheme detection and mounted state handling

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ import {
 } from '@mantine/core';
 import type { Session } from 'better-auth';
 import type { AppType } from 'next/app';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { api } from '~/utils/api';
 
@@ -13,10 +13,25 @@ const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
+
   const [colorScheme, setColorScheme] = useState<ColorScheme>('light');
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    const systemScheme = window.matchMedia('(prefers-color-scheme: dark)')
+      .matches
+      ? 'dark'
+      : 'light';
+    setColorScheme(systemScheme);
+    setMounted(true);
+  }, []);
 
   const toggleColorScheme = (value?: ColorScheme) =>
     setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+
+  if (!mounted) {
+    return null;
+  }
 
   return (
     <ColorSchemeProvider
