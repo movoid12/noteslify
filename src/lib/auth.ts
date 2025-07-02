7304b32f8faa5193575ef258f01e05051610126a
Mouaz Aldakkak <87415713+movoid12@users.noreply.github.com>
2025-07-02 13:12:01 +0200

    Revert "fix(auth): set default baseURL across auth and client modules"
    This reverts commit 94cfa2a07b16e1b51801f2efce469845a766ad9f.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,9 +6,8 @@ import { db } from '~/server/db';
 // import * as schema from '../server/db/schema';
 
 export const auth = betterAuth({
-  //baseURL: process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : env.BETTER_AUTH_URL,
   // biome-ignore lint/style/useNamingConvention: <explanation>
-  baseURL: env.BETTER_AUTH_URL,
+  baseURL: process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : env.BETTER_AUTH_URL,
   database: drizzleAdapter(db, {
     provider: 'pg',
     // schema: schema
--- src/lib/clients.ts
+++ src/lib/clients.ts
@@ -1,11 +1,10 @@
 import { createAuthClient } from 'better-auth/react';
-import { env } from '~/env';
 
-/* const baseUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : env.BETTER_AUTH_URL; */
+const baseUrl = process.env.VERCEL_URL 
+  ? `https://${process.env.VERCEL_URL}` 
+  : process.env.BETTER_AUTH_URL ;
 
 // biome-ignore lint/style/useNamingConvention: <explanation>
-export const authClient = createAuthClient({ baseURL: env.BETTER_AUTH_URL });
+export const authClient = createAuthClient({ baseURL: baseUrl });
 
 export const { signIn, signOut, useSession, getSession } = authClient;
