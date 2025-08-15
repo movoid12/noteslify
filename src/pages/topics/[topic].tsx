a67bf7cfdc7aa64de038a9f475c47c1db2a259eb
Mouaz Aldakkak <87415713+movoid12@users.noreply.github.com>
2025-08-15 13:24:31 +0200

    cleanup

--- src/pages/topics/[topic].tsx
+++ src/pages/topics/[topic].tsx
@@ -1,24 +1,14 @@
 import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
 
-import Head from 'next/head';
-
 const CategoryPage: NextPage = () => {
   const router = useRouter();
   const routedTopic = router.query.topic?.toString();
 
   return (
     <>
-      <Head>
-        <title>Noteslify App</title>
-        <meta
-          name="description"
-          content="Noteslify an app that makes your more creative to make"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <main>
-        <h1>Category Page</h1>
+        <h1>Category Page BOOOOOOO</h1>
         <h2>{routedTopic}</h2>
       </main>
     </>
